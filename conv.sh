2e040c2cfefb2407386bc663cee665e99a9225e4
Joel Kaaberg <joel.kaberg@gmail.com>
2018-06-10 11:53:22 +0000

    minor adjustments

--- conv.sh
+++ conv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# USAGE: conv.sh INPUT_DIRECTORY INPUT_FILE.EXT
+set -x
 
 # Parameters to conv.sh
 INPUT_FILE=$1
@@ -11,13 +11,13 @@ filename=$(basename -- "$INPUT_FILE")
 ext="${filename##*.}"
 filename="${filename%.*}"
 
-BASE_CMD="docker run --entrypoint "/usr/local/bin/ffmpeg" -v $INPUT_FILE:/input/$filename.$ext -v $OUTPUT_DIR:/output jrottenberg/ffmpeg"
+BASE_CMD="/usr/bin/docker run --rm -v ${INPUT_FILE}:/input/${filename}.${ext}:ro -v ${OUTPUT_DIR}:/output:rw jrottenberg/ffmpeg"
 
 # Options we will run FFMPEG with (max 720x406, 30fps)
-FFMPEG_OPTS="-c:v mpeg4 -vf scale=720:406:decrease -vtag xvid -qscale:v 5 -c:a libmp3lame -qscale:a 5"
+FFMPEG_OPTS="-c:v mpeg4 -vf scale=720:406:decrease -vtag xvid -qscale:v 2 -c:a libmp3lame -qscale:a 5"
 
 # Make sure output directory exists
-mkdir -p "$OUTPUT_DIR"
+mkdir -p "${OUTPUT_DIR}"
 
 # Do conversion
-"$BASE_CMD -y -i /input/$filename.$ext $FFMPEG_OPTS /output/$filename.avi"
+eval "${BASE_CMD} -y -i /input/${filename}.${ext} ${FFMPEG_OPTS} /output/${filename}.avi"
